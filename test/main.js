8856738b3f1e96df73da5c7a969b4e26a4fad171
UshioShugo <ushio.s@gmail.com>
2015-06-17 19:28:20 +0900

    \\ <- added

--- test/main.js
+++ test/main.js
@@ -10,6 +10,12 @@ describe('Samba Path Translater.', function(){
         translater.isWindows.should.be.ok
     })
 
+    it('SmbTranslater windows or not -> windows 2', function(){
+        var translater = new SmbTranslater('\\\\10.0.0.1\\foo\\var\\')
+
+        translater.isWindows.should.be.ng
+    })
+
     it('SmbTranslater windows or not -> not windows', function(){
         var translater = new SmbTranslater('smb://10.0.0.1/foo/var/')
 
@@ -31,6 +37,12 @@ describe('Samba Path Translater.', function(){
 
     it('SmbTranslater get not windows path', function(){
         var translater = new SmbTranslater('¥¥10.0.0.1¥foo¥var¥')
+
+        assert.equal(translater.getNotWindowsPath(), 'smb://10.0.0.1/foo/var/')
+    })
+
+    it('SmbTranslater get not windows path 2', function(){
+        var translater = new SmbTranslater('\\\\10.0.0.1\\foo\\var\\')
 
         assert.equal(translater.getNotWindowsPath(), 'smb://10.0.0.1/foo/var/')
     })
